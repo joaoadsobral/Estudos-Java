e7c736c6c452c4cf0c337e3812bb3f734efb78de
João Antônio <120070923+joaoadsobral@users.noreply.github.com>
2024-04-07 23:39:40 -0300

    Add files via upload

---
+++ 3 - Vetores, Arrays e Listas/Vetores - Exercício 1,  negativos/application/ProgramaNegativos.java
@@ -0,0 +1,26 @@
+package application;
+
+import java.util.Scanner;
+
+public class ProgramaNegativos {
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Quantos números serão digitados? ");
+        int n = input.nextInt();
+        int[] vect = new int[n];
+
+        for(int i=0; i<vect.length; i++){
+            System.out.println("Digite um número : ");
+            vect[i] = input.nextInt();
+        }
+
+        System.out.println("NÚMEROS NEGATIVOS");
+
+        for(int i=0; i<vect.length; i++){
+            if (vect[i] < 0){
+                System.out.println(vect[i]);
+            }
+        }
+    }
+}
---
+++ 3 - Vetores, Arrays e Listas/Vetores - Exercício 2, alturas/application/ProgramaAlturas.java
@@ -0,0 +1,52 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Pessoa;
+
+public class ProgramaAlturas {
+    public static void main(String[] args){
+
+        Locale.setDefault(Locale.US);
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Quantas pessoas serao digitadas?");
+        int n = input.nextInt();
+        Pessoa[] vect = new Pessoa[n];
+
+        for (int i=0; i<vect.length; i++){
+            System.out.printf("Dados da %da  pessoa:%n", i + 1);
+            System.out.println("Nome :");
+            input.nextLine();
+            String nome = input.nextLine();
+            System.out.println("Idade :");
+            int idade = input.nextInt();
+            System.out.println("Altura :");
+            double altura = input.nextDouble();
+
+            vect[i] = new Pessoa(nome, idade, altura);
+        }
+
+        int dadosIdades = 0;
+        double soma_altura = 0;
+        for (int i=0; i<vect.length; i++){
+            soma_altura += vect[i].getAltura();
+
+            if (vect[i].getIdade() < 16){
+                dadosIdades += 1;
+            }
+        }
+
+        double media = soma_altura / vect.length;
+        double porcentagem = (double) (dadosIdades * 100) / vect.length;
+        System.out.printf("Altura média : %.2f%n", media);
+        System.out.printf("Pessoas com idade menor que 16 anos = %.1f%%\n", porcentagem );
+
+        for (int i=0; i<vect.length; i++){
+            if (vect[i].getIdade() < 16){
+                System.out.printf("%s%n", vect[i].getNome());
+            }
+        }
+    }
+}
